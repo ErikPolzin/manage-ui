8b071d48f30fb125b65f89a57d5f29ab31fb733d
Keegan White <keeganthomaswhite@gmail.com>
2024-04-11 14:01:48 +0200

    added auth and errors to services page

--- ui/src/pages/ServicesPage.jsx
+++ ui/src/pages/ServicesPage.jsx
@@ -3,31 +3,47 @@ import Box from "@mui/material/Box";
 import NavBar from "../components/NavBar";
 import Alert from "@mui/material/Alert";
 import AddServiceDialogue from "../components/AddServiceDialogue";
-import {Fab} from "@mui/material";
+import {Card, CardContent, Fab, ListItemText} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import Footer from "../components/Footer";
 import ServiceList from "../components/ServiceList";
 import EditServiceDialogue from "../components/EditServiceDialogue";
 
+import { useKeycloak } from "@react-keycloak/web";
 function ServicesPage(){
+    const { keycloak } = useKeycloak();
     const [services, setServices] = useState([]);
     const [successAlert, setSuccessAlert] = useState({ show: false, message: '' });
     const [openAddDialogue, setOpenAddDialogue] = useState(false);
     const [alert, setAlert] = useState({ show: false, message: '' });
     const apiUrl = `${process.env.REACT_APP_API_URL}`
     const [originalName, setOriginalName] = useState('')
+    const [error, setError] = useState(null);
     const fetchServices = () => {
-        fetch(`${apiUrl}/service/list/`, {})
-            .then(response => response.json())
+        fetch(`${apiUrl}/service/list/`, {
+            headers: {
+                'Authorization': `Bearer ${keycloak.token}`,
+            },
+        })
+            .then(async response => {
+                if (!response.ok) {
+
+                    const errorBody = await response.json();
+                    throw new Error(errorBody.detail || "An unknown error occurred");
+                }
+                return response.json();
+            })
             .then(data => {
                 setServices(data.data);
             })
             .catch(error => {
                 console.error('Error:', error);
+                setError(error.message);
             });
     };
 
     useEffect(() => {
+        setError(null); // Clear any existing errors
         fetchServices();
     }, []); // Dependencies array is empty to indicate this effect runs once on mount.
 
@@ -58,9 +74,12 @@ function ServicesPage(){
     };
     const handleAddService = (newService) => {
         fetch(`${apiUrl}/service/add/`, {
+
             method: 'POST',
             headers: {
+                'Authorization': `Bearer ${keycloak.token}`,
                 'Content-Type': 'application/json',
+
             },
             body: JSON.stringify(newService),
         })
@@ -84,6 +103,7 @@ function ServicesPage(){
         fetch(`${apiUrl}/service/edit/${originalName}/`, { // Assuming your service has an 'id' field
             method: 'PATCH',
             headers: {
+                'Authorization': `Bearer ${keycloak.token}`,
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(updatedService),
@@ -105,7 +125,47 @@ function ServicesPage(){
             });
     };
 
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
 
+    if (!services.length) {
+        return (
+            <Box sx={{ minHeight: '100vh', backgroundColor: '#26282d', display: 'flex', flexDirection: 'column' }}>
+                <NavBar />
+                <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 2, margin: 2 }}>
+                    <Card sx={{
+                        margin: 2,
+                        maxWidth: 600,
+                        bgcolor: '#1e2022',
+                        boxShadow: 3,
+                    }}>
+                        <CardContent>
+                            <ListItemText primary={'There are no services registered.'} />
+                        </CardContent>
+                    </Card>
+                </Box>
+                <Fab
+                    onClick={() => setOpenAddDialogue(true)}
+                    color="button_green"
+                    aria-label="add"
+                    sx={{
+                        position: 'fixed',
+                        bottom: '95px',
+                        right: 10,
+                    }}
+                >
+                    <AddIcon />
+                </Fab>
+                <AddServiceDialogue
+                    open={openAddDialogue}
+                    handleClose={() => setOpenAddDialogue(false)}
+                    handleAdd={handleAddService}
+                />
+                <Footer />
+            </Box>
+        );
+    }
 
     return (
         <Box
