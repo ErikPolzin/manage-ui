2478b01de5aead609264b5ee669e2eb028082173
Erik Polzin <eriktpol@gmail.com>
2024-08-03 13:58:43 +0200

    Add accounts table to user page, tweak data usage pie layout

--- src/components/graphs/DataUsagePie.jsx
+++ src/components/graphs/DataUsagePie.jsx
@@ -1,65 +1,156 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import ToggleButton from "@mui/material/ToggleButton";
+import Stack from "@mui/material/Stack";
+import { useTheme } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
 import { PieChart } from "@mui/x-charts/PieChart";
+import { blueberryTwilightPalette } from "@mui/x-charts/colorPalettes";
+import { lighten } from "@mui/material/styles";
 
-function DataUsagePie({ users, selectedIndex }) {
-  const [dataType, setDataType] = React.useState("recv");
+function DataUsagePie({ accounts }) {
+  const theme = useTheme();
+  const colors = blueberryTwilightPalette(theme.palette.mode);
+  const [userColorTable, setUserColorTable] = React.useState({});
+  const [userIndex, setUserIndex] = React.useState(0);
 
-  const sendData = () =>
-    users.map((u) => ({
-      id: u.id,
-      value: u.bytes_sent,
-      label: u.username,
-    }));
+  const usernames = React.useCallback(
+    () => Array.from(new Set(accounts.map((ra) => ra.username))),
+    [accounts],
+  );
 
-  const recvData = () =>
-    users.map((u) => ({
-      id: u.id,
-      value: u.bytes_recv,
-      label: u.username,
-    }));
+  const lightenPercent = React.useCallback(
+    (u, acct) => {
+      if (!acct) return 0;
+      let acctsForUser = accounts.filter((a) => a.username === u);
+      if (acctsForUser.length === 0) return 0;
+      let indexOfAccount = acctsForUser.map((a) => a.radacctid).indexOf(acct);
+      return (indexOfAccount + 1) / acctsForUser.length / 2;
+    },
+    [accounts],
+  );
+
+  const colorForUser = React.useCallback(
+    (u, acct) => {
+      let userColor;
+      if (!userColorTable[u]) {
+        userColor = colors[userIndex % colors.length];
+        setUserColorTable({ ...userColorTable, [u]: userColor });
+        setUserIndex(userIndex + 1);
+      } else {
+        userColor = userColorTable[u];
+      }
+      return lighten(userColor, lightenPercent(u, acct));
+    },
+    [colors, lightenPercent, userColorTable, userIndex],
+  );
+
+  const sendData = React.useCallback(
+    () =>
+      accounts.map((u) => ({
+        id: u.radacctid,
+        value: u.acctinputoctets,
+        arcLabel: u.username,
+        color: colorForUser(u.username, u.radacctid),
+      })),
+    [accounts, colorForUser],
+  );
+
+  const sendDataGrouped = React.useCallback(
+    () =>
+      usernames().map((u) => ({
+        id: u,
+        value: accounts
+          .filter((ra) => ra.username === u)
+          .reduce((x, y) => x + y.acctinputoctets, 0),
+        arcLabel: u,
+        label: u,
+        color: colorForUser(u),
+      })),
+    [accounts, usernames, colorForUser],
+  );
+
+  const recvData = React.useCallback(
+    () =>
+      accounts.map((u) => ({
+        id: u.radacctid,
+        value: u.acctoutputoctets,
+        arcLabel: u.username,
+        color: colorForUser(u.username, u.radacctid),
+      })),
+    [accounts, colorForUser],
+  );
+
+  const recvDataGrouped = React.useCallback(
+    () =>
+      usernames().map((u) => ({
+        id: u,
+        value: accounts
+          .filter((ra) => ra.username === u)
+          .reduce((x, y) => x + y.acctoutputoctets, 0),
+        arcLabel: u,
+        color: colorForUser(u),
+      })),
+    [accounts, usernames, colorForUser],
+  );
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyItems: "center",
-        flexDirection: "column",
-        flexGrow: 1,
-      }}
-    >
-      <PieChart
-        highlightedItem={
-          selectedIndex !== -1 ? { seriesId: "send-recv", dataIndex: selectedIndex } : null
-        }
-        height={300}
-        series={[
-          {
-            data: dataType === "recv" ? recvData() : sendData(),
-            id: "send-recv",
-            innerRadius: 70,
-            cornerRadius: 3,
-            paddingAngle: 1,
-            highlightScope: { faded: "series", highlighted: "item" },
-            faded: { additionalRadius: -10, color: "gray" },
-          },
-        ]}
-        margin={{ top: 20, bottom: 10 }}
-      />
-      <ToggleButtonGroup
-        color="primary"
-        value={dataType}
-        onChange={(e, v) => v !== null && setDataType(v)}
-        exclusive
-        size="small"
-      >
-        <ToggleButton value={"recv"}>Received</ToggleButton>
-        <ToggleButton value={"send"}>Sent</ToggleButton>
-      </ToggleButtonGroup>
-    </Box>
+    <Stack direction={{ xs: "column-reverse", md: "row" }} width="100%" textAlign="center" spacing={2}>
+      <Box flexGrow={1}>
+        <Typography variant="h6">Received Data</Typography>
+        <Divider />
+        <PieChart
+          // highlightedItem={{ seriesId: "recv-grouped", dataIndex: selectedRecvIndex() }}
+          height={300}
+          series={[
+            {
+              innerRadius: 0,
+              outerRadius: 90,
+              id: "recv-grouped",
+              highlightScope: { faded: "series", highlighted: "item" },
+              faded: { additionalRadius: -10, color: "gray" },
+              data: recvDataGrouped(),
+            },
+            {
+              innerRadius: 95,
+              outerRadius: 120,
+              id: "recv-items",
+              highlightScope: { faded: "series", highlighted: "item" },
+              faded: { additionalRadius: -10, color: "gray" },
+              data: recvData(),
+            },
+          ]}
+          margin={{ top: 20, bottom: 10 }}
+        />
+      </Box>
+      <Box flexGrow={1}>
+        <Typography variant="h6">Sent Data</Typography>
+        <Divider />
+        <PieChart
+          // highlightedItem={{ seriesId: "send-grouped", dataIndex: selectedSendIndex() }}
+          height={300}
+          series={[
+            {
+              innerRadius: 0,
+              outerRadius: 90,
+              id: "send-grouped",
+              highlightScope: { faded: "series", highlighted: "item" },
+              faded: { additionalRadius: -10, color: "gray" },
+              data: sendDataGrouped(),
+            },
+            {
+              innerRadius: 95,
+              outerRadius: 120,
+              id: "send-items",
+              highlightScope: { faded: "series", highlighted: "item" },
+              faded: { additionalRadius: -10, color: "gray" },
+              data: sendData(),
+            },
+          ]}
+          margin={{ top: 20, bottom: 10 }}
+        />
+      </Box>
+    </Stack>
   );
 }
 
--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,63 +1,182 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListSubheader from "@mui/material/ListSubheader";
 import Divider from "@mui/material/Divider";
 import Avatar from "@mui/material/Avatar";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import ToggleButton from "@mui/material/ToggleButton";
 import { useTheme } from "@mui/material";
+import ConfirmationNumberIcon from "@mui/icons-material/ConfirmationNumber";
+import PersonIcon from "@mui/icons-material/Person";
 
 import DataUsagePie from "../components/graphs/DataUsagePie";
 import { fetchAPI } from "../keycloak";
+import ConnectedClientsList from "../components/ConnectedClientsList";
+import { MS_IN } from "../components/graphs/utils";
+
+function AccountsGroup({ groupName, accounts, onClick, selected, isVoucher, ...props }) {
+  return [
+    <ListItemButton {...props} selected={selected} onClick={onClick} key={`${groupName}-item`}>
+      <ListItemAvatar>
+        <Avatar sx={{ width: 24, height: 24 }}>
+          {!isVoucher ? (
+            <PersonIcon fontSize="small" />
+          ) : (
+            <ConfirmationNumberIcon fontSize="small" />
+          )}
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText primary={groupName} />
+    </ListItemButton>,
+    <Divider key={`${groupName}-divider`}></Divider>,
+  ];
+}
 
 function UsersPage() {
-  const [users, setUsers] = useState([]);
-  const [selectedUser, setSelectedUser] = useState(null);
+  const [allAccounts, setAllAccounts] = React.useState([]);
+  const [selectedAccount, setSelectedAccount] = React.useState(null);
+  const [minTime, setMinTime] = React.useState(null);
+  const [showDays, setShowDays] = React.useState("all");
   const theme = useTheme();
-
-  useEffect(() => {
-    fetchAPI("/accounts/users/")
+  // Sync min time when the user changes the toggle button
+  React.useEffect(() => {
+    if (showDays === "all") {
+      setMinTime(null);
+    } else {
+      setMinTime(new Date() - MS_IN[showDays]);
+    }
+  }, [showDays]);
+  // Fetch account info on page load
+  React.useEffect(() => {
+    fetchAPI("/radius/accounts/")
       .then((data) => {
-        setUsers(data);
+        setAllAccounts(data);
       })
       .catch((error) => {
         console.error("Error fetching users:", error);
       });
   }, []); // Fetch users on page load
-
-  const selectedIndex = () => {
-    return users.map((u) => u.id).indexOf(selectedUser);
-  };
+  /**
+   * The clients that take place (at least partially) in the current time range.
+   */
+  const accounts = React.useCallback(
+    () => allAccounts.filter((acc) => new Date(acc.acctstoptime || new Date()) > (minTime || 0)),
+    [allAccounts, minTime],
+  );
+  /**
+   * If an account group is selected, only show data from that account.
+   */
+  const visibleAccounts = React.useCallback(
+    () => accounts().filter((acc) => acc.username === selectedAccount || !selectedAccount),
+    [accounts, selectedAccount],
+  );
+  /**
+   * All the accounts that are associated with a user (e.g. username/password login)
+   */
+  const userAccounts = React.useCallback(
+    () => accounts().filter((ra) => !ra.is_voucher),
+    [accounts],
+  );
+  /**
+   * All the accounts that are associated with a voucher (e.g. voucher code login)
+   */
+  const voucherAccounts = React.useCallback(
+    () => accounts().filter((ra) => ra.is_voucher),
+    [accounts],
+  );
+  /** The names of the user accounts */
+  const usernames = React.useCallback(
+    () => Array.from(new Set(userAccounts().map((ra) => ra.username))),
+    [userAccounts],
+  );
+  /** The names of the voucher accounts */
+  const vouchernames = React.useCallback(
+    () => Array.from(new Set(voucherAccounts().map((ra) => ra.username))),
+    [voucherAccounts],
+  );
 
   return (
     <Stack direction={{ xs: "column", md: "row-reverse" }}>
-      <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
-        <DataUsagePie users={users} selectedIndex={selectedIndex()} />
-      </Box>
+      <Stack sx={{ width: "100%" }}>
+        <Box>
+          <DataUsagePie
+            accounts={visibleAccounts()}
+            usernames={usernames()}
+            vouchernames={vouchernames()}
+            selectedAccount={selectedAccount}
+          />
+        </Box>
+        <Box>
+          <ConnectedClientsList
+            clients={visibleAccounts()}
+            minTimeOverride={minTime}
+            maxTimeOverride={new Date()}
+          />
+        </Box>
+      </Stack>
       <Box
         sx={{
           [theme.breakpoints.up("md")]: {
             borderRight: `1px solid ${theme.palette.divider}`,
             height: "calc(100vh - 64px)",
           },
+          minWidth: 300,
         }}
       >
-        <List>
-          {users.map((user, i) => [
-            <ListItemButton
-              key={user.id}
-              selected={selectedUser === user.id}
-              onClick={(e) => setSelectedUser(selectedUser !== user.id ? user.id : null)}
-            >
-              <ListItemAvatar>
-                <Avatar sx={{ width: 24, height: 24 }} />
-              </ListItemAvatar>
-              <ListItemText primary={user.username} />
-            </ListItemButton>,
-            <Divider key={`${user.id}-divider`}></Divider>,
-          ])}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            paddingY: 1,
+          }}
+        >
+          <ToggleButtonGroup
+            color="primary"
+            value={showDays}
+            onChange={(e, v) => v !== null && setShowDays(v)}
+            exclusive
+            size="small"
+            aria-label="Date Range"
+          >
+            <ToggleButton value={"day"}>24 Hours</ToggleButton>
+            <ToggleButton value={"week"}>Week</ToggleButton>
+            <ToggleButton value={"month"}>Month</ToggleButton>
+            <ToggleButton value={"all"}>All</ToggleButton>
+          </ToggleButtonGroup>
+        </Box>
+        <Divider />
+        <List subheader={<ListSubheader>Users</ListSubheader>}>
+          {usernames().map((username) => (
+            <AccountsGroup
+              groupName={username}
+              isVoucher={false}
+              accounts={accounts().filter((ra) => ra.username === username)}
+              key={username}
+              selected={selectedAccount === username}
+              onClick={(e) => setSelectedAccount(selectedAccount !== username ? username : null)}
+            />
+          ))}
+        </List>
+        <List subheader={<ListSubheader>Vouchers</ListSubheader>}>
+          {vouchernames().map((vouchername) => (
+            <AccountsGroup
+              groupName={vouchername}
+              isVoucher={true}
+              accounts={accounts().filter((ra) => ra.username === vouchername)}
+              key={vouchername}
+              selected={selectedAccount === vouchername}
+              onClick={(e) =>
+                setSelectedAccount(selectedAccount !== vouchername ? vouchername : null)
+              }
+            />
+          ))}
         </List>
       </Box>
     </Stack>
